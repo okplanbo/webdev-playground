02ba7ac89357702da4b08a6c1645443d5d73ca98
okplanb <26038255+ok-plan-b@users.noreply.github.com>
2023-06-10 01:51:54 +0400

    tooling: linters, husky, mui, front page styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,58 @@
-import './App.css'
+import { Paper, Typography } from '@mui/material';
+
+import './App.scss'
 
 function App() {
-  // check mui, rafc, routing
-  // styleling + eslint + tsc
-  // husky commit hook
-  // add scss or styled
-
+  // add routes, rafc new lazy module
+  // do i really need vite-plugin-stylelint?
+  // check postcss stuff, autoprefixer
+  // use grid for cards
+  // tailwind? deploy gh pages or vercel
+  // add link to my gists to personal site
+  
   return (
     <>
-      <h1>Playground</h1>
-      <div className="card">
-        <h2>Stack:</h2>
-        <ul>
-          <li>Vite</li>
-          <li>React</li>
-          <li>Typescript</li>
-          <li>MUI</li>
-        </ul>
-      </div>
-      <div className="card">
-        <h2>Pages:</h2>
-        <ul>
-          <li>Uno</li>
-          <li>Dos</li>
-          <li>Tres</li>
-        </ul>
-      </div>
+      <Typography variant="h2" component="h1">
+        Webdev Playground
+      </Typography>
+      <Paper
+        variant="outlined"
+        className='container'
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          marginTop: '3em',
+        }}
+      >
+        <div className="card">
+          <Typography variant="h4" component="h2">
+            Stack
+          </Typography>
+          <ul>
+            <li>Vite + React + TS</li>
+            <li>MUI</li>
+            <li><a href='https://github.com/sindresorhus/modern-normalize' target='_blank'>Modern Normalize</a></li>
+            <li>PostCSS, Sass</li>
+            <li>Stylelint, ESlint</li>
+            <li>Husky</li>
+          </ul>
+        </div>
+        <div className="card">
+          <Typography variant="h4" component="h2">
+            Pages
+          </Typography>
+          <ul>
+            <li>ToDo</li>
+            <li>Uno</li>
+            <li>Dos</li>
+            <li>Tres</li>
+          </ul>
+        </div>
+      </Paper>
+      <footer>
+        <p>2023</p>
+        <p><a href='https://github.com/ok-plan-b/' target='_blank'>github.com/ok-plan-b</a></p>
+      </footer>
     </>
   )
 }
