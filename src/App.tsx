6f055130cbd585cecc3ab82c91ee3c81d0b2afbf
okplanb <okplanb42@gmail.com>
2023-06-09 15:50:23 +0400

    initial

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import './App.css'
+
+function App() {
+  // check mui, rafc, routing
+  // styleling + eslint + tsc
+  // husky commit hook
+  // add scss or styled
+
+  return (
+    <>
+      <h1>Playground</h1>
+      <div className="card">
+        <h2>Stack:</h2>
+        <ul>
+          <li>Vite</li>
+          <li>React</li>
+          <li>Typescript</li>
+          <li>MUI</li>
+        </ul>
+      </div>
+      <div className="card">
+        <h2>Pages:</h2>
+        <ul>
+          <li>Uno</li>
+          <li>Dos</li>
+          <li>Tres</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
